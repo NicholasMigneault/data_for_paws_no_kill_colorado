5bee7772c2ab36c9c582efa0bf41da0f24754363
NickMigneault <nicholas.migneault@curaleaf.com>
2025-09-21 10:32:13 -0400

    tweak source name for active licenses, update string formatting

--- dbt_nokillcolorado/models/source/google_sheets/src_google_sheets__active_licenses.sql
+++ dbt_nokillcolorado/models/source/google_sheets/src_google_sheets__active_licenses.sql
@@ -2,17 +2,23 @@ with formatting as (
     select
         -- PK
         -- columns
-        lower(account_name) as account_name
-        ,lower(dba) as dba
-        ,business_license_app_category_name as license_type
+        account_name as account_name
+        ,dba as dba
+        ,business_license_app_category_name as license_category
+        ,case when contains(license_category,'Sanctuary') then 'Sanctuary'
+            when contains(license_category,'Rescue') then 'Rescue'
+            when contains(license_category,'Shelter') then 'Shelter'
+            when contains(license_category,'Breeder') then 'Breeder'
+            else 'Other'
+            end as license_type
         ,strptime(expire_date, '%m/%d/%Y')::date as license_expiration_date
-        ,lower(city) as city
+        ,city
         ,state
-        ,lower(county) as county
-    from {{ source('google_sheets', 'statistics_licenses') }}
+        ,county
+    from {{ source('google_sheets', 'gsheet_active_licenses') }}
 )
 select 
-    {{ dbt_utils.generate_surrogate_key(['account_name', 'dba', 'license_type']) }} as active_license_sk
+    {{ dbt_utils.generate_surrogate_key(['account_name', 'dba', 'license_category']) }} as active_license_sk
     ,{{ dbt_utils.generate_surrogate_key(['account_name', 'dba', 'city', 'county', 'state']) }} as facility_sk
     ,*
 from formatting
